caf9db1dad7be809373dacb27f99534256fc67e6
Gary M. Josack <gary@byoteki.com>
2014-10-28 10:31:21 -0700

    Add sync command.

--- src/bin/sync.rs
+++ src/bin/sync.rs
@@ -1,3 +1,46 @@
+extern crate libc;
+extern crate getopts;
+
+extern crate coreutils;
+
+
+use std::os;
+
+
+extern {
+    fn sync() -> libc::c_void;
+}
+
+
 fn main() {
-    // TODO(gary): Rust doesn't expose sync. Revisit.
+
+    let args = os::args();
+    let program = args[0].clone();
+
+    let opts = [
+        getopts::optflag("h", "help", "display this help and exit"),
+        getopts::optflag("V", "version", "output version information and exit"),
+    ];
+
+    let matches = match getopts::getopts(args.tail(), opts) {
+        Ok(m) => { m },
+        Err(f) => {
+            println!("{}", f.to_string());
+            println!("-------------------------------");
+            println!("Usage: {}", getopts::usage(program.as_slice(), opts));
+            return;
+        },
+    };
+
+    if matches.opt_present("help") {
+        println!("Usage: {}", getopts::usage(program.as_slice(), opts));
+        return;
+    }
+
+    if matches.opt_present("version") {
+        println!("{}: {}", program, coreutils::version());
+        return;
+    }
+
+    unsafe { sync(); }
 }
